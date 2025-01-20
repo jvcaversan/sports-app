0484bb911935113f1f45a56109e42818572cc3dc
Joao Vitor Caversan <joaovitorcaversan@gmail.com>
2025-01-20 16:01:43 -0300

    adicionando debounce nas reqs

--- src/api/club_members/index.ts
+++ src/api/club_members/index.ts
@@ -1,5 +1,6 @@
 import { supabase } from "@/database/supabase";
 import { useQuery } from "@tanstack/react-query";
+import { useDebounce } from "use-debounce";
 
 export const useClubMembers = (clubId: string) => {
   return useQuery({
@@ -18,20 +19,22 @@ export const useClubMembers = (clubId: string) => {
 };
 
 export const useClubMembersByQuery = (clubId: string, searchQuery: string) => {
+  const [debouncedQuery] = useDebounce(searchQuery, 300);
+
   return useQuery({
-    queryKey: ["club_members", searchQuery],
+    queryKey: ["club_members", debouncedQuery],
     queryFn: async () => {
       const { data, error } = await supabase
         .from("club_members")
         .select("*")
         .eq("club_id", clubId)
-        .ilike("name", `%${searchQuery}%`);
+        .ilike("name", `%${debouncedQuery}%`);
       if (error) {
         throw new Error(error.message);
       }
       return data;
     },
-    enabled: searchQuery.length > 2,
+    enabled: debouncedQuery.length > 2,
     refetchOnWindowFocus: false,
     staleTime: 5000,
     retry: false,
