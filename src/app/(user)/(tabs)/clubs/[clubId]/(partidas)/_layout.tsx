7c5107bce0f49f8d7c71823cab2937283c03ff53
Joao Vitor Caversan <joaovitorcaversan@gmail.com>
2025-01-24 18:43:07 -0300

    ajustes tabela

--- src/app/(user)/(tabs)/clubs/[clubId]/(partidas)/_layout.tsx
+++ src/app/(user)/(tabs)/clubs/[clubId]/(partidas)/_layout.tsx
@@ -4,7 +4,6 @@ export default function PartidasLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="[id]" />
-      <Stack.Screen name="match-status" />
     </Stack>
   );
 }
